70bd4f1cde87b69bb5a04186153ea9279ada9a64
tagpunitkr <tagpunitkumar@gmail.com>
2023-07-08 15:04:54 +0530

    Added animations

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
     return (
-      <div className="w-full">
+      <div className="w-full absolute top-0">
         <ul className="flex justify-center p-4 bg-gray-900">
           <li> <button className="mx-16 bg-green-500 p-2 rounded-lg hover:bg-purple-500 active:bg-purple-700 font-bold">All Gigs</button></li>
           <li> <button className="mx-16 bg-green-500 p-2 rounded-lg hover:bg-purple-500 active:bg-purple-700 font-bold">My Gigs</button></li>
--- frontend/src/routes/HomePage/index.jsx
+++ frontend/src/routes/HomePage/index.jsx
@@ -1,25 +1,65 @@
 import { useNavigate } from 'react-router-dom';
-import Footer from "../../components/Footer"
+import { motion } from "framer-motion";
+import Footer from "../../components/Footer";
 
 export default function HomePage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function navigateToAllGigs() {
-        navigate('/gigs');
-    }
+  function navigateToAllGigs() {
+    navigate('/gigs');
+  }
 
-    return (
-        <>
-            <div className="flex flex-col justify-center items-center w-full h-screen font-semibold text-6xl bg-gray-900">
-                <div className="text-center space-y-5">
-                    <h1 className="text-green-500">Welcome to</h1>
-                    <h2 className="text-purple-500">Lazy App</h2>
-                </div>
-                <div className="text-center">
-                    <button onClick={navigateToAllGigs} className="text-xl bg-green-600 w-25 h-10 my-10 px-3 rounded-xl hover:bg-amber-500">Start</button>
-                </div>
-            </div>
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center w-full h-screen font-semibold text-6xl bg-gray-900">
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          className="text-center space-y-5"
+        >
+          <motion.h1
+            className="text-green-500"
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+          >
+            Welcome to
+          </motion.h1>
+          <motion.h2
+            className="text-purple-500"
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, delay: 0.4 }}
+          >
+            Lazy App
+          </motion.h2>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.6 }}
+          className="text-center"
+        >
+          <motion.button
+            onClick={navigateToAllGigs}
+            className="text-xl bg-green-600 w-25 h-10 my-10 mx-2 px-3 rounded-xl hover:bg-green-500 "
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+          Login 
+          </motion.button>
+          <motion.button
+            onClick={navigateToAllGigs}
+            className="text-xl bg-green-600 w-25 h-10 my-10 mx-2 px-3 rounded-xl hover:bg-green-500 "
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            >
+              SignUp
+          </motion.button>
+        </motion.div>
+      </div>
+      <Footer />
+    </>
+  );
 }
