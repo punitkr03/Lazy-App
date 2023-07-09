fadb4b473acb13007c89cea47fceab245908b86a
tagpunitkr <tagpunitkumar@gmail.com>
2023-07-09 23:36:07 +0530

    Theme

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-white dark:bg-gray-800 flex flex-col items-center">
+        <footer className=" bg-gray-800 flex flex-col items-center">
             <div className="w-full p-4 absolute bottom-0">
             <div className="w-full text-sm text-cyan-500 font-semibold text-center">
                 Made with love ❤️ by team VOID</div>
