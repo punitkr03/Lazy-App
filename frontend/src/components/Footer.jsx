57259d35ebda65483167d95b3355c1653aabf768
Punit Kumar <tagpunitkumar@gmail.com>
2023-09-23 19:26:27 +0530

    minor fix

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className=" bg-gray-800 flex flex-col items-center">
       <div className="w-full p-4 absolute bottom-0">
         <div className="w-full text-sm text-cyan-500 font-semibold text-center">
-          Made with love ❤️ by team VOID
+          Made with love ❤️ by Punit Kumar
         </div>
       </div>
     </footer>
